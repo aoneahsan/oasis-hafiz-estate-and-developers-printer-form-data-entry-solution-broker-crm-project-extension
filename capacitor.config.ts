6beac09942852d56697426baa733735d52b8b594
Ahsan Mahmood <aoneahsan@gmail.com>
2024-05-26 18:18:03 +0500

    before deleting and readding the android folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.zaions.invoiceGenerator',
-  appName: 'invoiceGenerator',
+  appId: 'com.zaions.oasis',
+  appName: 'oasis',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
